1864264f4fc33ad5927617d2b1834a7d4078f824
Yogesh More <iyogesh.more1998@gmail.com>
2022-06-07 12:24:31 +0530

    Added :Ability to create Employee Payroll Object On Save.

--- payroll_form.js
+++ payroll_form.js
@@ -7,7 +7,7 @@ salary.addEventListener('input', function() {
     output.textContent = salary.value;
     });
 
-//  <!--  Day 44-UC 2 – On Document Load Set Event Listeners.-->
+// <!-- Day 44-UC 2 – On Document Load Set Event Listeners. -->
 
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
@@ -27,3 +27,54 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 
+// <!-- Day 44-UC 3 On Save Create Employee Payroll Object. -->
+
+const save = () => {
+    try {
+      let employeePayrollData = createEmployeePayroll();
+    }
+    catch (e) {
+      return;
+    }
+  }
+  const createEmployeePayroll = () => {
+      let employeePayrollData = new EmployeePayrollData();
+      try {
+        employeePayrollData.name = getInputValueById('#name');
+      }
+      catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+      }
+    
+      employeePayrollData.profileImage = getSelectedValues('[name=profile]').pop();
+      employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+      employeePayrollData.department = getSelectedValues('[name=department]');
+      employeePayrollData.salary = getInputValueById("#salary");
+      employeePayrollData.notes = getInputValueById("#notes");
+    
+      let date = getInputValueById("#year") + "-" + getInputValueById("#month") + "-" + getInputValueById("#day");
+      employeePayrollData.date = Date.parse(date); 
+    
+      alert(employeePayrollData.toString());
+      return employeePayrollData;
+    }
+  
+    const getSelectedValues = (propertyValue) => {
+      let allItems = document.querySelectorAll(propertyValue);
+      let selItems = [];
+      allItems.forEach(item => {
+        if (item.checked) selItems.push(item.value);
+      });
+      return selItems;
+    };
+    
+    const getInputValueById = (id) => {
+      let value = document.querySelector(id).value;
+      return value;
+    };
+    
+    const getInputElementValue = (id) => {
+      let value = document.getElementById(id).value;
+      return value;
+    };
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+difference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+        // if (difference > 30 || difference < 0) {
+        //   throw "Start Date is Invalid";
+        // } else {
+        //   this._startDate = startDate;
+        // }
