6ba9ab12111323b6ca30becb6de461a380d7b68d
Yogesh More <iyogesh.more1998@gmail.com>
2022-06-07 11:51:24 +0530

    Added :Ability to set Event Listeners when Document is loaded .

--- Employee _Payroll_Data.js
+++ Employee _Payroll_Data.js
@@ -1,24 +1,13 @@
-// <!-- UC 1 – Employee Payroll Class with New Attributes. -->
-
 class EmployeePayrollData {
 
-    constructor(...params) {
-        this.name = params[0];
-        this.profileImage = params[1];
-        this.gender = params[2];
-        this.department = params[3];
-        this.salary = params[4];
-        this.startDate = params[5];
-        this.notes = params[6];
-        this.id = params[7];
+    get id() {
+        return this._id;
     }
 
-    get id() {
-        return this.id;
-    }
     set id(id) {
         this._id = id;
     }
+
     get name() {
         return this._name;
     }
@@ -28,7 +17,7 @@ class EmployeePayrollData {
         if (nameRegex.test(name))
             this._name = name;
         else
-            throw "**** NAME is Incorrect ****";
+            throw "Name is Incorrect";
     }
 
     get profileImage() {
@@ -69,8 +58,13 @@ class EmployeePayrollData {
     }
 
     set startDate(startDate) {
-            this._startDate = startDate;
-
+        let difference = Date.now() - startDate;
+        difference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+        if (difference > 30 || difference < 0) {
+          throw "Start Date is Invalid";
+        } else {
+          this._startDate = startDate;
+        }
     }
 
     get notes() {
@@ -85,52 +79,6 @@ class EmployeePayrollData {
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
-        return "Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
-    }
-}
-
-function save() {
-
-    let employeeName = document.querySelector('#name').value;
-    let profileList = document.querySelectorAll('input[name="profile"]');
-    let employeeProfileImage;
-    for (let image of profileList) {
-        if (image.checked) {
-            employeeProfileImage = image.value;
-            break;
-        }
-    }
-
-    let genderList = document.querySelectorAll('input[name="gender"]');
-    let employeeGender;
-    for (let gender of genderList) {
-        if (gender.checked) {
-            employeeGender = gender.value;
-            break;
-        }
-    }
-
-    let departmentList = document.querySelectorAll('.checkbox:checked');
-    let employeeDepartment = new Array();
-    for (let department of departmentList) {
-        if (department.checked) {
-            employeeDepartment.push(department.value);
-        }
-    }
-
-    let employeeSalary = document.querySelector('#salary').value;
-
-    let day = document.querySelector('#day').value;
-    let month = document.querySelector('#month').value;
-    let year = document.querySelector('#year').value;
-    let employeeStartDate = new Date(year, month, day);
-
-    let employeeNotes = document.querySelector('#notes').value;
-
-    try {
-        let employeePayrollData = new EmployeePayrollData(employeeName, employeeProfileImage, employeeGender, employeeDepartment, employeeSalary, employeeStartDate, employeeNotes);
-        console.log(employeePayrollData.toString());
-    } catch (e) {
-        console.error(e);
+        return "Id = " + this.id + ", Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
     }
 }
--- payroll_form.js
+++ payroll_form.js
@@ -4,5 +4,26 @@ const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function() {
-output.textContent = salary.value;
+    output.textContent = salary.value;
+    });
+
+//  <!--  Day 44-UC 2 – On Document Load Set Event Listeners.-->
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+      if (name.value.length == 0) {
+        nameError.textContent = "";
+        return;
+      }
+      try {
+        (new EmployeePayrollData()).name = name.value;
+        nameError.textContent = "";
+      }
+      catch (e) {
+        nameError.textContent = e;
+      }
+    });
 });
+
